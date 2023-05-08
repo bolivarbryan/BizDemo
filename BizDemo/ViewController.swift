56e0055d17712ddbbf0172ccc4408887c9347cb7
Bryan Bolivar <bryan.andres.bolivar@gmail.com>
2023-05-08 13:07:38 -0500

    Initial Commit

---
+++ BizDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BizDemo
+//
+//  Created by Bryan A Bolivar M on 8/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
